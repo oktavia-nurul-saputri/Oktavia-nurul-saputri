7bbb8efb386035031ffcf3561d1f4a9651a1219f
oktavia-nurul-saputri <pkm19@teknokrat.ac.id>
2025-07-14 16:25:20 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('1OiPZDTgX52-IExqwxvNhigPWbTwdCIiFFR_CgFrKsqI')
 config_artikel.gid('710523043')
 
 
